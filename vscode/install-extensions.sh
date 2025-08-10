89907f39cb10d4b37a3e5a989994bdecc42ddf4d
Afonso Dutra Nogueira Filho <afonsoft@gmail.com>
2025-08-10 20:38:53 -0300

    Ajustes

--- vscode/install-extensions.sh
+++ vscode/install-extensions.sh
@@ -21,18 +21,13 @@ echo "Instalando extensões..."
 /app/code-server/bin/code-server --install-extension esbenp.prettier-vscode
 /app/code-server/bin/code-server --install-extension gformat.html-formatter
 /app/code-server/bin/code-server --install-extension github.copilot
-/app/code-server/bin/code-server --install-extension github.copilot-chat
 /app/code-server/bin/code-server --install-extension github.vscode-github-actions
 /app/code-server/bin/code-server --install-extension github.vscode-pull-request-github
-/app/code-server/bin/code-server --install-extension johnpapa.angular-essentials
 /app/code-server/bin/code-server --install-extension johnpapa.angular2
 /app/code-server/bin/code-server --install-extension johnpapa.vscode-peacock
 /app/code-server/bin/code-server --install-extension johnpapa.winteriscoming
 /app/code-server/bin/code-server --install-extension shd101wyy.markdown-preview-enhanced
-/app/code-server/bin/code-server --install-extension visualstudioexptteam.intellicode-api-usage-examples
-/app/code-server/bin/code-server --install-extension visualstudioexptteam.vscodeintellicode
 /app/code-server/bin/code-server --install-extension vscode-icons-team.vscode-icons
 /app/code-server/bin/code-server --install-extension zhuangtongfa.material-theme
 
 echo "Iniciando o code-server..."
-/app/code-server/bin/code-server --bind-addr 0.0.0.0:8443 /workspace
